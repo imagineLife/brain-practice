35d90a8415cb4931be57693b64b6d3b5b0d7ccda
Eric Laursen <ericjlaursen@gmail.com>
2020-01-23 19:54:44 -0500

    an intro file

---
+++ 1/index.js
@@ -0,0 +1,12 @@
+const brain = require('brain.js')
+const trainingData = require('./data.json')
+// input 0 0, output 0
+// input 0 1, output 1
+// input 1 0, output 1
+// input 1 1, output 0
+
+const net = new brain.NeuralNetwork({ hiddenLayers: [3] });
+
+net.train(trainingData);
+
+console.log(net.run([0, 0]));
